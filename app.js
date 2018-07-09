11dd759b984d1af31969914ba7d6be5fea9cf83e
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-09 11:01:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
-app.post('https://mst-line-bot.herokuapp.com', (req, res) => res.sendStatus(200))
+app.post('https://mst-line-bot.herokuapp.com/', (req, res) => res.sendStatus(200))
 app.listen(port)
