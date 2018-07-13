0553904b415660289c148f5831b24c52b5aeb15b
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-13 11:03:11 +0700

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
-app.post('https://mst-line-bot.herokuapp.com/', (req, res) => res.sendStatus(200))
-app.listen(port)
