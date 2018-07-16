52067bb90de7db97055783c22e048efa30bd36c2
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-16 17:29:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -83,22 +83,30 @@ function handleEvent(event) {
   switch (event.type) {
     case 'message':
       const message = event.message;
-      switch (message.type) {
-        case 'text':
-          return handleText(message, event.replyToken, event.source);
-        case 'image':
-          return handleImage(message, event.replyToken, event.source);
-        case 'video':
-         return handleVideo(message, event.replyToken);
-        case 'audio':
-          return handleAudio(message, event.replyToken);
-        case 'location':
-          return handleLocation(message, event.replyToken);
-        case 'sticker':
-          return handleSticker(message, event.replyToken);
-        default:
-          throw new Error(`Unknown message: ${JSON.stringify(message)}`);
-      }
+      return client.replyMessage(
+        replyToken,
+        {
+          type: 'text',
+         text : message.type
+        }
+      );
+
+      // switch (message.type) {
+      //   case 'text':
+      //     return handleText(message, event.replyToken, event.source);
+      //   case 'image':
+      //     return handleImage(message, event.replyToken, event.source);
+      //   case 'video':
+      //    return handleVideo(message, event.replyToken);
+      //   case 'audio':
+      //     return handleAudio(message, event.replyToken);
+      //   case 'location':
+      //     return handleLocation(message, event.replyToken);
+      //   case 'sticker':
+      //     return handleSticker(message, event.replyToken);
+      //   default:
+      //     throw new Error(`Unknown message: ${JSON.stringify(message)}`);
+      // }
 
     case 'follow':
       return replyText(event.replyToken, 'Got followed event');
