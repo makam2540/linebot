086aec61418e6b74a6c618dc6ec3327b9dcfbbc1
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-17 11:35:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -301,7 +301,7 @@ function handleText(message, replyToken, source) {
       );
     default:
       console.log(`Echo message to ${replyToken}: ${message.text}`);
-      return replyText(replyToken, "message.text");
+      return replyText(replyToken, "what ?");
   }
 }
 
