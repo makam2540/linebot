72d51d0ae5c862b71ee03b9e2a32dc5fb20f3ec3
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-18 09:47:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -399,20 +399,21 @@ function handleAudio(message, replyToken) {
       // var getDuration = require("get-audio-duration");
       var audioDuration;
       
-      getDuration(downloadPath)
-        .then((duration) => { audioDuration = duration; })
-        .catch((error) => { audioDuration = 1; })
-        .finally(() => {
+      // getDuration(downloadPath)
+      //   .then((duration) => { audioDuration = duration; })
+      //   .catch((error) => { audioDuration = 1; })
+      //   .finally(() => {
 
           return client.replyMessage(
             replyToken,
             {
               type: 'audio',
               originalContentUrl: baseURL + '/downloaded/' + path.basename(downloadPath),
-              duration: audioDuration * 1000,
+              duration: 6000
+              // duration: audioDuration * 1000,
             }
           );
-        });
+        // });
     });
 }
 
