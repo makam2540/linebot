d203749279d3be6f17f95ef2089eb03bf6d7e23d
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-20 16:31:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -398,55 +398,32 @@ function handleVideo(message, replyToken, source) {
     });
 }
 
-function initSound(arrayBuffer) {
-    var base64String = bufferToBase64(arrayBuffer);
-    var audioFromString = base64ToBuffer(base64String);
-    document.getElementById("audio1").value=base64String;
-    context.decodeAudioData(audioFromString, function (buffer) {
-        // audioBuffer is global to reuse the decoded audio later.
-        audioBuffer = buffer;
-        var buttons = document.querySelectorAll('button');
-        buttons[0].disabled = false;
-        buttons[1].disabled = false;
-    }, function (e) {
-        console.log('Error decoding file', e);
-    });
-}
 
 function handleAudio(message, replyToken) {
 
-    const downloadPath = path.join(__dirname, 'downloaded', `${message.id}.mp3`);
+  const downloadPath = path.join(__dirname, 'downloaded', `${message.id}.mp3`);
 
-    return downloadContent(message.id, downloadPath)
-      .then((downloadPath) => {
-  
-        var originalUrl = baseURL + '/downloaded/' + path.basename(downloadPath)
+  return downloadContent(message.id, downloadPath)
+    .then((downloadPath) => {
 
+      var originalUrl = baseURL + '/downloaded/' + path.basename(downloadPath)
 
-        originalUrl.addEventListener('change', function (e) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
+          return client.replyMessage(
+            replyToken,
+            {
 
-                return client.replyMessage(
-                    replyToken,
-                    {
+              type: 'text',
+              text : originalUrl
+
+              // type: 'audio',
+              // originalContentUrl: baseURL + '/downloaded/' + path.basename(downloadPath),
+              // duration: 1000
+              // duration: duration * 1000,
+            }
+          );
         
-                      type: 'text',
-                      text : oinitSound(this.result)
-        
-                    }
-                  );
-
-                
-            };
-            reader.readAsArrayBuffer(this.files[0]);
-        }, false);
-  
-      
-
-          
-      });
-  }
+    });
+}
 
 function downloadContent(messageId, downloadPath) {
   return client.getMessageContent(messageId)
