551bd693372f13801ca307ebc4dc18f1bc3c6888
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-20 14:15:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -396,11 +396,11 @@ function handleAudio(message, replyToken) {
   return downloadContent(message.id, downloadPath)
     .then((downloadPath) => {
 
-      var originalUrl = baseURL + '/downloaded/' + path.basename(downloadPath)
+      // var originalUrl = baseURL + '/downloaded/' + path.basename(downloadPath)
      
-      var file = fs.createWriteStream("teerasej.mp3");
+      var file = fs.createWriteStream("8292019837101.mp3");
  
-      var request = http.get(originalUrl, function(response) {
+      var request = http.get("https://sangster-bot.herokuapp.com/downloaded/8292019837101.mp3", function(response) {
               response.pipe(file);
               file.on('finish', function() {
                   file.close();
