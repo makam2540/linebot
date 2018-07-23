90b70c9fd9f34e35af8b5e1b8dcf93a8607490a8
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-23 15:01:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const context = require('audio-context')();
 
     // var date = Date.UTC()
     var date = new Date();
-    var s = date.getTimezoneOffset() / (-60)
-    var a = date + s
+    var a = date.getTimezoneOffset() 
+    
     // var a = date.toGMTString()
 // create LINE SDK config from env variables
 const config = {
