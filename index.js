08f92b746289db4f6b3c182ed9c32225ca0754e7
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-23 14:33:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const context = require('audio-context')();
                       }                      
     };
 
-    var date = Date().getTime()
+    var date = Date.UTC()
 
 // create LINE SDK config from env variables
 const config = {
