bc92d982e8a57b060a92ab97abbf519b73c8f069
makam2540 <40285474+makam2540@users.noreply.github.com>
2018-07-23 11:31:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -325,7 +325,7 @@ function handleText(message, replyToken, source) {
     var uid = source.userId
      
     var gid = source.groupId
-      var d = Date
+      var d = Date()
     var conn = new sql.ConnectionPool(dbConfig);
         conn.connect().then(function() {
               var req = new sql.Request(conn); 
